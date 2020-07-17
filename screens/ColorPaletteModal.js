4ae217d7d99039466a0a685bdd4446e2fdb8c112
infantito <danielinfantito61@gmail.com>
2020-07-16 23:32:14 -0500

    add: a input field and submit button

--- screens/ColorPaletteModal.js
+++ screens/ColorPaletteModal.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+import { Alert } from 'react-native';
+
+const Field = styled.TextInput`
+  border: 1.5px solid #282c34;
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+const View = styled.View`
+  flex: 1;
+  padding: 10px;
+`;
+
+const Label = styled.Text`
+  margin-bottom: 5px;
+`;
+
+const Submit = styled.TouchableOpacity`
+  align-items: center;
+  background-color: #61dafb;
+  border-radius: 5px;
+  justify-content: center;
+  margin-top: 50px;
+  padding: 12px 10px;
+`;
+
+const SubmitText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+`;
 
 const ColorPaletteModal = props => {
+  const [name, setName] = useState('');
+
+  const handleSubmit = useCallback(() => {
+    if (!name.trim()) {
+      Alert.alert('Please enter a palette name 🎨');
+    }
+  }, [name]);
+
   return (
     <View>
-      <Text>👋 🌎!</Text>
+      <Label>Name of the palette 🎨</Label>
+      <Field onChangeText={setName} value={name} placeholder="Palette name" />
+      <Submit onPress={handleSubmit}>
+        <SubmitText>Submit</SubmitText>
+      </Submit>
     </View>
   );
 };
